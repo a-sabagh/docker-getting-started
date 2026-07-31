83dd41fc7c15afe9601123c6a94dee962c971e3e
a-sabagh <a.sabagh72@gmail.com>
2026-07-31 13:54:24 +0330

    Add phonebook entrypoint example

---
+++ dockerfile-entrypoint-phonebook/docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e 
+
+touch /root/data.txt
+
+exec "$@"
---
+++ dockerfile-entrypoint-phonebook/phonebook.php
@@ -0,0 +1,30 @@
+#!/usr/bin/env php
+<?php
+
+$stdin = fopen('php://stdin', 'r');
+
+while(true) {
+    echo "Enter Phone|exit: ";
+
+    $in = fgets($stdin);
+
+    $in = trim($in);
+
+    if ('exit' === $in) {
+        echo "🆗 EXIT \n";
+
+        break;
+    }
+
+    sleep(1);
+
+    file_put_contents(
+        '/root/data.txt', 
+        $in . PHP_EOL, 
+        FILE_APPEND
+    );
+
+    echo "saved {$in}" . PHP_EOL;
+}
+
+fclose($stdin);
