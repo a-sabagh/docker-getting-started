5fef050970894ba7e5c27cb24a81611bbfb1a1fb
a-sabagh <a.sabagh72@gmail.com>
2026-08-13 12:10:08 +0330

    expose instruction

---
+++ docker-expose-php-server/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+
+    <style>
+        body {
+            min-height: 100vh;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        h1 {
+            margin: 0;
+            padding: 20px 30px;
+            border: 2px solid #333;
+            border-radius: 8px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome PHP Server</h1>
+</body>
+</html>
