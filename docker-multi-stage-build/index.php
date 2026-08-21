d9211a16f85980a3ae3099c851916076159b5829
a-sabagh <a.sabagh72@gmail.com>
2026-08-21 19:12:49 +0330

    simple file uploader

---
+++ docker-multi-stage-build/index.php
@@ -0,0 +1,45 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use SimpleUploader\Uploader;
+
+$uploadedFiles = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $uploadDirectory = __DIR__ . '/uploads';
+
+    $uploader = new Uploader($uploadDirectory);
+
+    $uploadedFiles = $uploader->upload($_FILES['files']);
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Simple File Uploader</title>
+    <style>
+        body { max-width: 40rem; margin: 3rem auto; font-family: sans-serif; }
+    </style>
+</head>
+
+<body>
+    <h1>Upload files</h1>
+
+    <form method="post" enctype="multipart/form-data">
+        <input type="file" name="files[]" multiple>
+        <button type="submit">Upload</button>
+    </form>
+
+    <?php if ($uploadedFiles): ?>
+        <h2>Uploaded</h2>
+        <ul>
+            <?php foreach ($uploadedFiles as $file): ?>
+                <li><a href="uploads<?php echo $file['path']; ?>"><?php echo $file['basename']; ?></a></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+</body>
+</html>
